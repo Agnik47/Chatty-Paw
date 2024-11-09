bad54ee230aed34fb5c7b24b53b99996e9cb9f4b
Agnik47 <140933190+Agnik47@users.noreply.github.com>
2024-11-09 21:35:43 +0530

    Add Profile page

--- src/components/LeftSidebar/Lsidebar.jsx
+++ src/components/LeftSidebar/Lsidebar.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import "./Lsidebar.css"
-import assets from '../../assets/assets'
+import React from "react";
+import "./Lsidebar.css";
+import assets from "../../assets/assets";
+import { Link } from "react-router-dom";
 const Lsidebar = () => {
   return (
-    <div className='ls'>
-            <div className="ls-top">
-                <div className="ls-nav">
-                    <img src={assets.logo} alt="" className='logo' />
-                    <div className="menu">
-                        <img src={assets.menu_icon} alt="" />
-                    </div>
-                </div>
-
-                <div className="ls-search">
-                    <img src={assets.search_icon} alt="" className='logo'/>
-                    <input type="text" placeholder='search here'/>
-                </div>
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="" className="logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+            <div className="sub-menu">
+              <Link to={"/profile"}>
+                <p>Edit Profile</p>
+              </Link>
+              <hr />
+              <p>Log Out</p>
             </div>
+          </div>
+        </div>
 
-            <div className="ls-list">
-              {Array(20).fill("").map((item,i)=> (
-                  <div key={i} className="friends">
-                  <img src={assets.profile_img} alt="" />
-                  <div>
-                      <p>D Shanmukha</p>
-                      <span>Abe Chal Nandi Hills chalte hai</span>
-                  </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" className="logo" />
+          <input type="text" placeholder="search here" />
+        </div>
+      </div>
+
+      <div className="ls-list">
+        {Array(20)
+          .fill("")
+          .map((item, i) => (
+            <div key={i} className="friends">
+              <img src={assets.profile_img} alt="" />
+              <div>
+                <p>D Shanmukha</p>
+                <span>Abe Chal Nandi Hills chalte hai</span>
               </div>
-              ))}
             </div>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Lsidebar
+export default Lsidebar;
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import "./ProfileUpdate.css"
+import React, { useState } from "react";
+import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
+  document.title = "Chatty Paws - Profile"
+  const [image, setImage] = useState(false);
   return (
-    <div>ProfileUpdate</div>
-  )
-}
+    <div className="profile">
+      <div className="prof-con">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            Upload Profile Image
+          </label>
+          <input type="text" placeholder="Your Name" required name="" id="" />
+          <textarea placeholder="Write Profile bio..." required></textarea>
+          <button>Submit</button>
+        </form>
 
-export default ProfileUpdate
+        <img src={image ? URL.createObjectURL(image) : assets.logo_icon} className="prof-pic " alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileUpdate;
